7e070ccd7d124613058c3e3dd31a90ce0f197ce0
JeanLiz004 <eljeanliz@hotmail.com>
2024-05-04 19:53:23 -0400

    Se agrega el proyecto

---
+++ API/Models/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace API.Models;
+
+public partial class Categoria
+{
+    public int IdCategoria { get; set; }
+
+    public string? Descripcion { get; set; }
+
+    [JsonIgnore]
+    public virtual ICollection<Producto> Productos { get; set; } = new List<Producto>();
+}
---
+++ API/Models/DbapiContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Models;
+
+public partial class DbapiContext : DbContext
+{
+    public DbapiContext()
+    {
+    }
+
+    public DbapiContext(DbContextOptions<DbapiContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Categoria> Categoria { get; set; }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Categoria>(entity =>
+        {
+            entity.HasKey(e => e.IdCategoria).HasName("PK__CATEGORI__A3C02A107ABEDAC2");
+
+            entity.ToTable("CATEGORIA");
+
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.IdProducto).HasName("PK__PRODUCTO__09889210ECDD7069");
+
+            entity.ToTable("PRODUCTO");
+
+            entity.Property(e => e.CodigoBarra)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Marca)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Precio).HasColumnType("decimal(10, 2)");
+
+            entity.HasOne(d => d.oCategoria).WithMany(p => p.Productos)
+                .HasForeignKey(d => d.IdCategoria)
+                .HasConstraintName("FK_IDCATEGORIA");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
